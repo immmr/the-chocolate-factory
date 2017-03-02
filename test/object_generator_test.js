3a7a4e3d9723b10694c06db6e4cb4974ed0f33df
Matthias Dumke <matthias.dumke@gmx.net>
2017-03-02 13:48:39 +0100

    rename generator to compiler

--- test/object_generator_test.js
+++ test/object_generator_test.js
@@ -1,19 +1,19 @@
 import {test} from 'tap'
-import ObjectGenerator from '../lib/object_generator'
+import TemplateCompiler from '../lib/template_compiler'
 
 test('create static objects', g => {
   g.test('creates a shallow static object', t => {
-    const generator = new ObjectGenerator()
+    const compiler = new TemplateCompiler()
     const template = {
       id: 1,
       name: 'Claire'
     }
-    t.same(generator.compile(template), template)
+    t.same(compiler.compile(template), template)
     t.end()
   })
 
   g.test('creates a nested static object', t => {
-    const generator = new ObjectGenerator()
+    const compiler = new TemplateCompiler()
     const template = {
       id: 1,
       name: 'Claire',
@@ -21,12 +21,12 @@ test('create static objects', g => {
         street: 'Sesame street 5'
       }
     }
-    t.same(generator.compile(template), template)
+    t.same(compiler.compile(template), template)
     t.end()
   })
 
   g.test('creates a deeply nested static object', t => {
-    const generator = new ObjectGenerator()
+    const compiler = new TemplateCompiler()
     const template = {
       id: 1,
       name: 'Claire',
@@ -37,7 +37,7 @@ test('create static objects', g => {
         }
       }
     }
-    t.same(generator.compile(template), template)
+    t.same(compiler.compile(template), template)
     t.end()
   })
   g.end()
@@ -45,7 +45,7 @@ test('create static objects', g => {
 
 test('create dynamic objects', g => {
   g.test('generates an {{ id }}', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       id: '{{ id }}'
     }
@@ -53,13 +53,13 @@ test('create dynamic objects', g => {
       generateId: rand => rand(10)
     }
 
-    const result = generator.compile(template, options)
+    const result = compiler.compile(template, options)
     t.equal(result.id, 3, `expected 3, got ${result.id}`)
     t.end()
   })
 
   g.test('generates a {{ name }}', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       name: '{{ name }}'
     }
@@ -67,13 +67,13 @@ test('create dynamic objects', g => {
       generateName: rand => ['Hugo', 'Derya', 'Ashish'][rand(3)]
     }
 
-    const result = generator.compile(template, options)
+    const result = compiler.compile(template, options)
     t.equal(result.name, 'Derya', `unexpected result ${result.name}`)
     t.end()
   })
 
   g.test('can deal with different whitespaces', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       name1: '{{  name }}',
       name2: '{{name  }}',
@@ -84,7 +84,7 @@ test('create dynamic objects', g => {
       generateName: rand => ['Hugo', 'Claire', 'Ashish'][rand(3)]
     }
 
-    const result = generator.compile(template, options)
+    const result = compiler.compile(template, options)
     t.equal(result.name1, 'Claire', `unexpected result ${result.name1}`)
     t.equal(result.name2, 'Claire', `unexpected result ${result.name2}`)
     t.equal(result.name3, 'Claire', `unexpected result ${result.name3}`)
@@ -93,7 +93,7 @@ test('create dynamic objects', g => {
   })
 
   g.test('compiles a complicated object', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       id: '{{ id }}',
       name: '{{ name }}',
@@ -109,7 +109,7 @@ test('create dynamic objects', g => {
       generateName: rand => ['Frank', 'Sinatra'][rand(2)],
       generateToken: rand => rand(20)
     }
-    const result = generator.compile(template, options)
+    const result = compiler.compile(template, options)
     const expectation = {
       id: 7,
       name: 'Frank',
@@ -126,22 +126,22 @@ test('create dynamic objects', g => {
   })
 
   g.test('throws an error if interpolation is attemped w/o options', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       id: '{{ id }}'
     }
 
-    t.throws(() => generator.compile(template), new Error('missing argument'))
+    t.throws(() => compiler.compile(template), new Error('missing argument'))
     t.end()
   })
 
   g.test('throws an error if callback-function is missing', t => {
-    const generator = new ObjectGenerator(1)
+    const compiler = new TemplateCompiler(1)
     const template = {
       id: '{{ id }}'
     }
 
-    t.throws(() => generator.compile(template, {}), new Error('could not find'))
+    t.throws(() => compiler.compile(template, {}), new Error('could not find'))
     t.end()
   })
 
