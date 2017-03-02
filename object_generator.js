a3fc3027726bcf3a9819b16f70b818b2fd0848ff
Matthias Dumke <matthias.dumke@gmx.net>
2017-03-02 10:27:35 +0100

    finish first working version of json-generator

--- object_generator.js
+++ object_generator.js
@@ -1,6 +1,32 @@
+import rand from 'random-seed'
+import {deepMap} from './utils'
+
 class ObjectGenerator {
-  compile (template) {
-    return template
+  constructor (seed) {
+    this.seed = seed || rand(10000)
+    this.rand = rand.create()
+  }
+
+  compile (template, options) {
+    return deepMap(template, value => {
+      const regEx = /__[^_]+__/g
+
+      if (typeof value === 'string' && value.match(/__[^_]+__/g)) {
+        return value.replace(/__[^_]+__/g, subString => {
+          // reduce '__id__' to 'id'
+          let variable = subString.match(/__(.+)__/)[1]
+
+          // and create full function name like 'generateId'
+          let fn = `generate${variable[0].toUpperCase() + variable.slice(1)}`
+
+          // call the function with freshly seeded rand-object
+          this.rand.seed(this.seed)
+          return options[fn](this.rand)
+        })
+      }
+
+      return value
+    })
   }
 }
 
--- object_generator_test.js
+++ object_generator_test.js
@@ -12,6 +12,99 @@ test('create static objects', g => {
     t.end()
   })
 
+  g.test('creates a nested static object', t => {
+    const generator = new ObjectGenerator()
+    const template = {
+      id: 1,
+      name: 'Claire',
+      address: {
+        street: 'Sesame street 5'
+      }
+    }
+    t.same(generator.compile(template), template)
+    t.end()
+  })
+
+  g.test('creates a deeply nested static object', t => {
+    const generator = new ObjectGenerator()
+    const template = {
+      id: 1,
+      name: 'Claire',
+      interests: {
+        recent: {
+          professional: 'painting',
+          hobby: 'nothing really'
+        }
+      }
+    }
+    t.same(generator.compile(template), template)
+    t.end()
+  })
   g.end()
 })
 
+test('create dynamic objects', g => {
+  g.test('generates an __id__', t => {
+    const generator = new ObjectGenerator(1)
+    const template = {
+      id: '__id__'
+    }
+    const options = {
+      generateId: rand => rand(10)
+    }
+
+    const result = generator.compile(template, options)
+    t.equal(result.id, '3', `expected 3, got ${result.id}`)
+    t.end()
+  })
+
+  g.test('generates a __name__', t => {
+    const generator = new ObjectGenerator(1)
+    const template = {
+      name: '__name__'
+    }
+    const options = {
+      generateName: rand => ['Hugo', 'Derya', 'Ashish'][rand(3)]
+    }
+
+    const result = generator.compile(template, options)
+    t.equal(result.name, 'Derya', `expected 'Derya', got ${result.name}`)
+    t.end()
+  })
+
+  g.test('compiles a complicated object', t => {
+    const generator = new ObjectGenerator(1)
+    const template = {
+      id: '__id__',
+      name: '__name__',
+      admin: false,
+      access: {
+        id: '__id__',
+        code: '__name__:__id__',
+        token: '__token__'
+      }
+    }
+    const options = {
+      generateId: rand => rand(20),
+      generateName: rand => ['Frank', 'Sinatra'][rand(2)],
+      generateToken: rand => rand(20)
+    }
+    const result = generator.compile(template, options)
+    const expectation = {
+      id: 7,
+      name: 'Frank',
+      admin: false,
+      access: {
+        id: 7,
+        code: 'Frank:7',
+        token: 7
+      }
+    }
+
+    t.same(result, expectation, `unexpected result ${JSON.stringify(result)}`)
+    t.end()
+  })
+
+  g.end()
+
+})
